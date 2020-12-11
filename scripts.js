ba428d8b13c200dc656205dc745b9f787e8e9a93
DanielAdeyemi <daniel@Daniel-A.local>
2020-12-10 16:34:43 -0800

    finish functions in JS

--- scripts.js
+++ scripts.js
@@ -1,23 +1,18 @@
 
-function no() {
-  $("#no").show();
-  $("#q1").show();
-  $("#r1").show();
-  $("#r2").show();
-}
-
-
 $(document).ready(function() {
   $("button#first").click(function() {
     $(".form-check").hide();
     $("#first").hide();
     $(".h2").hide();
+
     if (document.getElementById("neg").checked === true) {
-      no();
+      $("#no").show();
+      $(".question").show();
     }
     else {
       $("#yes").show();   
     }
+  
   var sel = $("#kchoice");
   sel.change(function() {
     var print = $(this).val();
@@ -31,18 +26,52 @@ $(document).ready(function() {
         }
         else {
           document.getElementById("end").innerHTML = print;
-        $("#result").show();
-        event.preventDefault();
+          $("#other").hide();
+          $("#personal").hide();
+          $("#kchoice").hide();
+          $("#finita").show();
+          event.preventDefault();
         }
         
-      });
-      
+      });  
     }
     else {
       document.getElementById("end").innerHTML = print;
-    $("#result").show();
+      $("#kchoice").hide();
+      $("#finita").show();
+    }   
+    });
+  });
+  
+  $("#finish").click(function(){
+    var a1 = document.getElementById("q1").value;
+    var a2 = document.getElementById("q2").value;
+    var a3 = document.getElementById("q3").value;
+    var a4 = document.getElementById("q4").value;
+    var a5 = document.getElementById("q5").value;
+    if (a1 === "default" || a2 === "default" || a3 === "default" || a4 === "default" || a5 === "default") {
+      return alert("You need to answer all questions! Try again");
+
+     }
+    else if (a1 === "q1a1" && a2 === "q2a5" && a3 === "q3a3") {
+      document.getElementById("end").innerHTML = "C#";
     }
-    
-  });
-  });
-});
+    else if (a1 === "q1a4" && a5 !== "q5a2" && a3 !== "q3a3") {
+      document.getElementById("end").innerHTML = "HTML";
+    }
+    else if (a1 === "q1a4" || a1 === "q1a3") { 
+      if (a2 !== "q2a2" && a3 === "q3a3") {
+      document.getElementById("end").innerHTML = "Ruby";
+      }
+      else {
+        document.getElementById("end").innerHTML = "Swift"; 
+      }
+    }  
+    else {
+      document.getElementById("end").innerHTML = "Python";
+    }
+
+    $("#no").hide();
+    $("#finita").show();
+     });
+    });
